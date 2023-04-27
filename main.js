9b32d816df61b534dab23c633fcb8ee4a466bc56
Askadhhh <127306364+Askadhhh@users.noreply.github.com>
2023-04-27 21:27:16 +0500

    запретил ввод текста при ошибке

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ const inputText = document.getElementById("inputText");
 const wrongInputText = document.getElementById("wrongInputText");
 const text = document.getElementById("text");
 
+let prevInputText = "";
+
 inputText.addEventListener("input", function (event) {
   if (text.innerText.startsWith(event.target.value)) {
     wrongInputText.innerText = "";
+    prevInputText = event.target.value;
   } else {
     wrongInputText.innerText = "Неправильно!";
+    event.target.value = prevInputText;
   }
 });
