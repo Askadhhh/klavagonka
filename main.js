4ed8513372702d9f27abdc1861cd5122cb8477f3
Askadhhh <127306364+Askadhhh@users.noreply.github.com>
2023-04-27 20:55:28 +0500

    зделал проверку введенного текста

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const inputText = document.getElementById("inputText");
+const wrongInputText = document.getElementById("wrongInputText");
+const text = document.getElementById("text");
+
+inputText.addEventListener("input", function (event) {
+  if (text.innerText.startsWith(event.target.value)) {
+    wrongInputText.innerText = "";
+  } else {
+    wrongInputText.innerText = "Неправильно!";
+  }
+});
